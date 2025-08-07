d12e8ce5788f7378e6bee731cddfb86c279186e0
tirthajagiya <tirthajagiya5618@gmail.com>
2025-08-07 18:48:45 +0530

    new

---
+++ Java/Lab_6_2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Lab_6_2 {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      int i,j;
+      for(i=0;i<3;i++){
+        for(j=0;j<=i;j++){
+            System.out.print(" *");
+        }
+        System.out.println("");
+      }
+    }}
